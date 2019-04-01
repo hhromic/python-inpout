495542b2bba86a55b2a481597be742904cad99dc
Hugo Hromic <hhromic@users.noreply.github.com>
2019-04-01 20:37:48 +0100

    cli: added new command-line tool: pprint

---
+++ inpout/cli.py
@@ -0,0 +1,31 @@
+# Simple input/output using MessagePack and LZ4 for Python
+# Copyright 2019 Hugo Hromic
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Command-line tools."""
+
+import sys
+from pprint import PrettyPrinter
+from .inpout import load_iter
+
+def pprint():
+    """Pretty-print files."""
+    pprinter = PrettyPrinter()
+    for fname in sys.argv[1:]:
+        sys.stderr.write("Processing file '%s' ...\n" % fname)
+        try:
+            for obj in load_iter(fname, use_list=False):
+                pprinter.pprint(obj)
+        except Exception as excp:  # pylint: disable=broad-except
+            sys.stderr.write("%s\n" % excp)
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 NAME = "inpout"
-VERSION = "1.0.0"
+VERSION = "1.0.1"
 DESCRIPTION = "Simple input/output using MessagePack and LZ4 for Python"
 AUTHOR = "Hugo Hromic"
 AUTHOR_EMAIL = "hhromic@gmail.com"
@@ -24,6 +24,11 @@ KEYWORDS = [
     "output",
     "library",
 ]
+ENTRY_POINTS = {
+    "console_scripts": [
+        "inpout-pprint = inpout.cli:pprint",
+    ],
+}
 LICENSE = "Apache-2.0"
 
 def _read_file(filename):
@@ -38,6 +43,7 @@ setup(
     classifiers=CLASSIFIERS,
     install_requires=REQUIRES,
     provides=[NAME],
+    entry_points=ENTRY_POINTS,
     keywords=KEYWORDS,
     license=LICENSE,
     platforms=["all"],
