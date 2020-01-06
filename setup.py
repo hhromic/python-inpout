75cf04af84413653a9b30eae2797d45e002e5e19
Hugo Hromic <hhromic@users.noreply.github.com>
2020-01-06 22:03:26 +0000

    New version release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 NAME = "inpout"
-VERSION = "1.0.7"
+VERSION = "1.0.8"
 DESCRIPTION = "Simple input/output using MessagePack and LZ4 for Python"
 AUTHOR = "Hugo Hromic"
 AUTHOR_EMAIL = "hhromic@gmail.com"
